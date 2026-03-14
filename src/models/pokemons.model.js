44cbeb82bffa78fe21e0fb2bfd1bd80e80103caa
felix gagne <2436915@carrefour.cegepvicto.ca>
2026-03-14 10:12:46 -0400

    ConvertionPG

--- src/models/pokemons.model.js
+++ src/models/pokemons.model.js
@@ -1,15 +1,15 @@
 import { parse } from 'dotenv';
-import pool from '../config/db.js';
+import pool from '../config/db_pg.js';
 
 const getPokemon = async (id) => {
 
-    const requete = `SELECT nom,type_primaire,type_secondaire,pv,attaque,defense FROM pokemon WHERE id = ? LIMIT 1`;
+    const requete = `SELECT nom,type_primaire,type_secondaire,pv,attaque,defense FROM pokemon WHERE id = $1 LIMIT 1`;
     const params = [id];
 
     try {
 
-        const [resultats] = await pool.query(requete, params);
-        return resultats[0] ?? null;
+        const resultats = await pool.query(requete, params);
+        return resultats.rows[0] ?? null;
 
     } catch (erreur) {
         console.log(`Erreur, code: ${erreur.code} sqlState ${erreur.sqlState} : 
@@ -19,7 +19,7 @@ const getPokemon = async (id) => {
 };
 const listeToutePokemons = async(type) =>{
         if(type){
-            var requete = `SELECT nom,type_primaire,type_secondaire,pv,attaque,defense FROM pokemon WHERE type_primaire = ? ORDER BY nom`;
+            var requete = `SELECT nom,type_primaire,type_secondaire,pv,attaque,defense FROM pokemon WHERE type_primaire = $1 ORDER BY nom`;
             var params = [type];            
         } else{
             var requete = `SELECT nom,type_primaire,type_secondaire,pv,attaque,defense FROM pokemon ORDER BY nom`;
@@ -28,12 +28,12 @@ const listeToutePokemons = async(type) =>{
 
     try {
         if(type){
-            var [resultats] = await pool.query(requete, params);
+            var resultats = await pool.query(requete, params);
         }else{
-            var [resultats] = await pool.query(requete);            
+            var resultats = await pool.query(requete);            
         }
 
-        return resultats ?? null;
+        return resultats.rows ?? null;
 
     } catch (erreur) {
         console.log(`Erreur, code: ${erreur.code} sqlState ${erreur.sqlState} : 
@@ -42,12 +42,12 @@ const listeToutePokemons = async(type) =>{
     }
 };
 const creerPokemonsbd = async(nom,type_primaire,type_secondaire,pv,attaque,defense) =>{
-    const requete = `INSERT INTO pokemon(nom,type_primaire,type_secondaire,pv,attaque,defense) VALUE (?,?,?,?,?,?)`;
+    const requete = `INSERT INTO pokemon(nom,type_primaire,type_secondaire,pv,attaque,defense) VALUES ($1,$2,$3,$4,$5,$6) RETURNING id`;
     const param = [nom,type_primaire,type_secondaire,pv,attaque,defense];
     try {
-        const [resultats] = await pool.execute(requete,param);
+        const resultats = await pool.query(requete,param);
 
-        return resultats.insertId ?? null;
+        return resultats.rows ?? null;
     } catch (erreur) {
         console.log(`Erreur, code: ${erreur.code} sqlState ${erreur.sqlState} : 
                     ${erreur.sqlMessage}`);
@@ -55,12 +55,12 @@ const creerPokemonsbd = async(nom,type_primaire,type_secondaire,pv,attaque,defen
     }
 }
 const modifierPokemonsbd = async(id,nom,type_primaire,type_secondaire,pv,attaque,defense) =>{
-    const requete = `UPDATE pokemon SET nom=?, type_primaire=?,type_secondaire=?,pv=?,attaque=?,defense=? WHERE id=?`;
+    const requete = `UPDATE pokemon SET nom=$1, type_primaire=$2,type_secondaire=$3,pv=$4,attaque=$5,defense=$6 WHERE id=$7 RETURNING id`;
     const param = [nom,type_primaire,type_secondaire,pv,attaque,defense,id];
     try {
-        const [resultats] = await pool.execute(requete,param);
+        const resultats = await pool.query(requete,param);
 
-        return resultats.insertId ?? null;
+        return resultats.rows ?? null;
     } catch (erreur) {
         console.log(`Erreur, code: ${erreur.code} sqlState ${erreur.sqlState} : 
                     ${erreur.sqlMessage}`);
@@ -69,12 +69,12 @@ const modifierPokemonsbd = async(id,nom,type_primaire,type_secondaire,pv,attaque
 }
 const supprimerPokemonsbd = async(id) =>{
 
-    const requete = `DELETE FROM pokemon WHERE id = ?`;
+    const requete = `DELETE FROM pokemon WHERE id = $1`;
     const param = [id];
     try {
-        const [resultats] = await pool.execute(requete,param);
+        const resultats = await pool.query(requete,param);
 
-        return resultats ?? null;
+        return resultats.rows ?? null;
     } catch (erreur) {
         console.log(`Erreur, code: ${erreur.code} sqlState ${erreur.sqlState} : 
                     ${erreur.sqlMessage}`);
@@ -86,13 +86,13 @@ const validerExistance = async(id) =>{
     {
         return null;
     }    
-    const requete = `SELECT nom FROM pokemon WHERE id = ?`;
+    const requete = `SELECT nom FROM pokemon WHERE id = $1`;
     const params = [id];
 
     try {
 
-        const [resultats] = await pool.query(requete, params);
-        return resultats[0] ?? null;
+        const resultats = await pool.query(requete, params);
+        return resultats.rows[0] ?? null;
 
     } catch (erreur) {
         console.log(`Erreur, code: ${erreur.code} sqlState ${erreur.sqlState} : 
